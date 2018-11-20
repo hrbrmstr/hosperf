b02b1476a788aff6973c32e0b7aec3f92934e2c0
boB Rudis <bob@rud.is>
2018-11-20 07:22:22 -0500

    R package repo initialization complete

---
+++ R/hosperf-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/hosperf>
+#' - BugReports: <https://gitlab.com/hrbrmstr/hosperf/issues>
+#' 
+#' @md
+#' @name hosperf
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
